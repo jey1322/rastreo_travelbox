0ffc714367a041c31ae66ba2376f8bc34561a378
Jairo Antonio Paniagua Garcia <jeypaniagua@gmail.com>
2025-08-02 22:32:05 -0600

    change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rastreo_travelbox/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
