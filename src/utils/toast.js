894f6b355ea51251143c541412970579fe454e56
Jairo Antonio Paniagua Garcia <jeypaniagua@gmail.com>
2025-08-02 22:53:43 -0600

    Updates

--- src/utils/toast.js
+++
@@ -1,8 +0,0 @@
-import { createToaster } from '@meforma/vue-toaster'
-
-export const toast = createToaster({
-  position: 'top',
-  duration: 3000,
-  max: 2,
-  pauseOnHover: false
-})
--- src/views/NotFoundView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="min-h-screen flex items-center justify-center uppercase text-gray-400">
-    <h1>Not Found</h1>
-  </div>
-</template>
-
-<script setup></script>
